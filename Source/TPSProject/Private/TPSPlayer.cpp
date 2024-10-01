1c659b4abb0d4c3dc604635b72110966aa2a7c0f
yshzjq <ysh10150@gmail.com>
2024-10-01 14:44:00 +0900

    TPSC++Comp설정

--- Source/TPSProject/Private/TPSPlayer.cpp
+++ Source/TPSProject/Private/TPSPlayer.cpp
@@ -3,6 +3,8 @@
 
 #include "TPSPlayer.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 // Sets default values
 ATPSPlayer::ATPSPlayer()
@@ -18,7 +20,30 @@ ATPSPlayer::ATPSPlayer()
 	// 위치 (X=0.000000,Y=0.000000,Z=70.000000)
 	SpringArmComp->SetRelativeLocation(FVector(0, 0, 70));
 
+	// Camera를 생성해서 SpringArm에 붙인다.
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+	CameraComp->SetupAttachment(SpringArmComp);
 	
+	// Mesh에 3D 에셋을 로드해서 넣어준다.
+	// 생성자 도우미를 이용해서 스켈레탈 메쉬를 로드한다.
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> tempMesh(TEXT("/Script/Engine.SkeletalMesh'/Game/ParagonWraith/Characters/Heroes/Wraith/Meshes/Wraith.Wraith'"));
+
+	//만약, 로드가 성공했다면
+	if (tempMesh.Succeeded())
+	{
+		GetMesh()->SetSkeletalMesh(tempMesh.Object);
+		// 메시를 넣은 다음에 캐릭터의 위치 변경과 회전을 했다.
+		// 위치 (X=0.000000,Y=0.000000,Z=-90.000000)
+		// 회전 (Pitch=0.000000,Yaw=-90.000000,Roll=0.000000)
+		GetMesh()->SetRelativeLocationAndRotation(FVector(0, 0, -90), FRotator(0, -90, 0));
+
+		bUseControllerRotationYaw = false;
+		SpringArmComp->bUsePawnControlRotation = true;
+		CameraComp->bUsePawnControlRotation = false;
+		GetCharacterMovement()->bOrientRotationToMovement = true;
+	}
+
+
 
 }
 
